21d16083571ace94b2076a013c7e35fd3665a869
unknown <295481044@qq.com>
2022-04-15 20:46:13 +0800

    feat:成功连接数据库

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,56 @@
 <template>
-  <MainLoading v-if="loading" />
-  <n-config-provider :local="zhCN" :date-locale="dateZhCN">
-    <!-- 日历视图 -->
-    <div id="calender" v-if="isCalendarDataPrepared">
-      <n-calendar
-        #="{ year, month, date }"
-        :default-value="calendarTime"
-        :value="calendarTime"
-      >
-        {{ year }}-{{ month }}-{{ date }}
-        <br>
-        <n-popover trigger="hover" v-for="(item, index) in getTodoList(year, month, date)" :key="index">
-          <template #trigger>
-            <span class="arrangement" v-if="item.isFinished === 0" :style="item.style">
-              {{item.title}}&ensp;&ensp;{{item.time}}
-              <br>
+  <n-message-provider>
+    <MainLoading v-if="loading" />
+    <n-config-provider :local="zhCN" :date-locale="dateZhCN">
+      <!-- 日历视图 -->
+      <div id="calender" v-if="isCalendarDataPrepared">
+        <n-calendar
+          #="{ year, month, date }"
+          :default-value="calendarTime"
+          :value="calendarTime"
+        >
+          {{ year }}-{{ month }}-{{ date }}
+          <br>
+          <n-popover trigger="hover" v-for="(item, index) in getTodoList(year, month, date)" :key="index">
+            <template #trigger>
+              <span class="arrangement" v-if="item.is_finished === 0" :style="'background-color: #' + item.style">
+                {{item.title}}&ensp;&ensp;{{item.time}}
+                <br>
+              </span>
+              <span class="arrangement" v-else-if="item.is_finished === 1" style="background-color: grey">
+                <del>{{item.title}}&ensp;&ensp;{{item.time}}</del>
+                <br>
+              </span>
+            </template>
+            <h2>{{item.title}}</h2>
+            <p>{{item.content}}</p>
+            <p>{{item.date}}&ensp;{{item.time}}</p>
+            <n-button v-if="item.is_finished === 0" quaternary type="primary" @click="changeFinish(item, 1)">
+              完成
+            </n-button>
+            <span v-else-if="item.is_finished === 1" style="color: #18A058">
+              已完成
             </span>
-            <span class="arrangement" v-else-if="item.isFinished === 1" style="background-color: grey">
-              <del>{{item.title}}&ensp;&ensp;{{item.time}}</del>
-              <br>
-            </span>
-          </template>
-          <h2>{{item.title}}</h2>
-          <p>{{item.content}}</p>
-          <p>{{item.date}}&ensp;{{item.time}}</p>
-          <n-button v-if="item.isFinished === 0" quaternary type="primary" @click="changeFinish(item, 1)">
-            完成
-          </n-button>
-          <span v-else-if="item.isFinished === 1" style="color: #18A058">
-            已完成
-          </span>
-        </n-popover>
-      </n-calendar>
-    </div>
-  </n-config-provider>
+          </n-popover>
+        </n-calendar>
+      </div>
+    </n-config-provider>
+  </n-message-provider>
 </template>
 
 <script>
-import { onMounted, reactive, ref } from 'vue'
+import { onMounted, onUnmounted, reactive, ref } from 'vue'
 import MainLoading from './components/MainLoading.vue'
-import { NCalendar, NConfigProvider, NPopover, NButton, darkTheme, zhCN, dateZhCN } from 'naive-ui'
+import { NCalendar, NConfigProvider, NPopover, NButton, NMessageProvider, darkTheme, zhCN, dateZhCN } from 'naive-ui'
+import sqlite3 from 'sqlite3'
+
+// 这个databaseUrl是打包的时候用的
+// const databaseUrl = '../bundled/easytodo_DB.db'
+
+// 这个databaseUrl是在开发的时候用的
+const databaseUrl = './easytodo_DB.db'
+
+var DB = null
 
 // 全局化配置
 function configHandler () {
@@ -53,7 +64,6 @@ function configHandler () {
 // 管理日历的函数
 function canlendarHandler () {
 
-  /* let data:Array<IToDoListItem> = [] */
   let data = reactive([])
 
   const time = Date.now()
@@ -61,15 +71,15 @@ function canlendarHandler () {
   const isCalendarDataPrepared = ref(false)
 
   onMounted(() => {
-    /* axios.get('/todoList_jsonDB.json').then((res:IResponseBody<IToDoListItem>) => {
-      if (res.data) {
-        data = res.data.data as Array<IToDoListItem>
+    DB.all('select * from todolist', (err, rows) => {
+      console.log('获取数据')
+      if (err) {
+        console.log(err)
+        return
       }
+      data = rows
       isCalendarDataPrepared.value = true
-    }).catch(err => {
-      console.log(err)
-    }) */
-    isCalendarDataPrepared.value = true
+    })
   })
 
   const getTodoList = (year, month, date) => {
@@ -90,9 +100,16 @@ function canlendarHandler () {
         break
       }
     }
-    // 用于刷新
-    isCalendarDataPrepared.value = false
-    isCalendarDataPrepared.value = true
+    let updateStr = 'update todo_list set is_finished = ' + flag + ' where id = ' + item.id
+    DB.run(updateStr, (err) => {
+      if (err) {
+        console.log(err)
+        return
+      }
+      // 用于刷新
+      isCalendarDataPrepared.value = false
+      isCalendarDataPrepared.value = true
+    })
   }
 
   return {
@@ -109,19 +126,41 @@ export default {
     NCalendar,
     NPopover,
     NConfigProvider,
+    NMessageProvider,
     NButton
   },
   setup () {
-    const loading = ref(true)
 
-    onMounted(() => {
-      setTimeout(() => {
-        loading.value = false
-      }, 1000)
-    })
+    /* window.$message = useMessage() */
+    
+    const loading = ref(true)
 
     const { calendarTime, getTodoList, isCalendarDataPrepared, changeFinish } = canlendarHandler()
     const { zhCN, dateZhCN } = configHandler()
+
+    DB = new sqlite3.Database(databaseUrl, (err) => {
+      if (err) {
+        console.error(err)
+      }
+      else{
+        // 成功之后关闭加载界面
+        setTimeout(() => {
+          loading.value = false
+        }, 3000)
+      }
+    })
+
+    /* onMounted(() => {
+      setTimeout(() => {
+        if (databaseFail) {
+          window.$message.error('抱歉，加载数据出现意外错误，请尝试重启程序')
+        }
+      }, 1000)   
+    }) */
+
+    onUnmounted(() => {
+      DB.close()
+    })
 
     return {
       loading,
@@ -166,5 +205,31 @@ export default {
     display: flex;
     justify-content: space-between;
     margin: 10px 0;
+  }
+  ::-webkit-scrollbar {
+  /*滚动条整体样式*/
+  width : 10px;  /*高宽分别对应横竖滚动条的尺寸*/
+  height: 1px;
+  }
+  ::-webkit-scrollbar-thumb {
+  /*滚动条里面小方块*/
+  border-radius   : 10px;
+  background-color: rgb(139, 139, 139);
+  background-image: -webkit-linear-gradient(
+      45deg,
+      rgba(255, 255, 255, 0.2) 25%,
+      transparent 25%,
+      transparent 50%,
+      rgba(255, 255, 255, 0.2) 50%,
+      rgba(255, 255, 255, 0.2) 75%,
+      transparent 75%,
+      transparent
+  );
+  }
+  ::-webkit-scrollbar-track {
+  /*滚动条里面轨道*/
+  box-shadow   : inset 0 0 5px rgba(0, 0, 0, 0.2);
+  background   : #ededed;
+  border-radius: 10px;
   }
 </style>
