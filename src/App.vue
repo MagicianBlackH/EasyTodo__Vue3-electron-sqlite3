553ef7fa5e34fc871abc28150874327b93af6008
unknown <295481044@qq.com>
2022-04-15 21:31:52 +0800

    fix:优化了数据的获取与更新

--- src/App.vue
+++ src/App.vue
@@ -25,12 +25,15 @@
             <h2>{{item.title}}</h2>
             <p>{{item.content}}</p>
             <p>{{item.date}}&ensp;{{item.time}}</p>
-            <n-button v-if="item.is_finished === 0" quaternary type="primary" @click="changeFinish(item, 1)">
-              完成
-            </n-button>
-            <span v-else-if="item.is_finished === 1" style="color: #18A058">
+            <span v-if="item.is_finished === 1" style="color: #18A058">
               已完成
             </span>
+            <n-button v-if="item.is_finished === 0" tertiary type="primary" @click="changeFinish(item, 1)">
+              完成
+            </n-button>
+            <n-button v-else-if="item.is_finished === 1" quaternary type="error" @click="changeFinish(item, 0)">
+              取消完成
+            </n-button>
           </n-popover>
         </n-calendar>
       </div>
@@ -39,7 +42,7 @@
 </template>
 
 <script>
-import { onMounted, onUnmounted, reactive, ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import MainLoading from './components/MainLoading.vue'
 import { NCalendar, NConfigProvider, NPopover, NButton, NMessageProvider, darkTheme, zhCN, dateZhCN } from 'naive-ui'
 import sqlite3 from 'sqlite3'
@@ -64,14 +67,39 @@ function configHandler () {
 // 管理日历的函数
 function canlendarHandler () {
 
-  let data = reactive([])
+  let data = []
 
   const time = Date.now()
   const calendarTime = ref(time)
   const isCalendarDataPrepared = ref(false)
 
+  // 从数据库获取 todolist 数据
+  function getTodoListFromDB () {
+    return new Promise((resolve, reject) => {
+      DB.all('select * from todolist', (err, rows) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(rows)
+        }
+      })
+    })
+  }
+
+  // 重新挂载数据
+  function refreshCalendarData () {
+    isCalendarDataPrepared.value = false
+    isCalendarDataPrepared.value = true
+  }
+
   onMounted(() => {
-    DB.all('select * from todolist', (err, rows) => {
+    getTodoListFromDB().then(res => {
+      data = res
+      isCalendarDataPrepared.value = true
+    }).catch(e => {
+      console.log(e)
+    })
+    /* DB.all('select * from todolist', (err, rows) => {
       console.log('获取数据')
       if (err) {
         console.log(err)
@@ -79,7 +107,7 @@ function canlendarHandler () {
       }
       data = rows
       isCalendarDataPrepared.value = true
-    })
+    }) */
   })
 
   const getTodoList = (year, month, date) => {
@@ -100,15 +128,19 @@ function canlendarHandler () {
         break
       }
     }
-    let updateStr = 'update todo_list set is_finished = ' + flag + ' where id = ' + item.id
+    let updateStr = 'update todolist set is_finished = ' + flag + ' where id = ' + item.id
     DB.run(updateStr, (err) => {
       if (err) {
         console.log(err)
         return
       }
-      // 用于刷新
-      isCalendarDataPrepared.value = false
-      isCalendarDataPrepared.value = true
+      getTodoListFromDB().then(res => {
+        data = res
+        // 用于刷新
+        refreshCalendarData()
+      }).catch(e => {
+        console.log(e)
+      })
     })
   }
 
